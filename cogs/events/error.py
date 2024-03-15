5a25e2cf351053f7b728af73b728066ea501015e
BeneX Drake <benex@hotmail.de>
2024-03-15 23:08:26 +0100

    log channel id from .env file

--- cogs/events/error.py
+++ cogs/events/error.py
@@ -1,14 +1,19 @@
 import discord
 from discord.ext import commands
 
+import os
+from dotenv import load_dotenv
+
+
 class Error(commands.Cog):
     
     def __init__(self, bot:discord.Bot):
         self.bot = bot
+        load_dotenv()
 
     @commands.Cog.listener()
     async def on_application_command_error(self,ctx:commands.Context,error):
-        channel = await self.bot.fetch_channel(1215205031205277767)
+        channel = await self.bot.fetch_channel(int(os.getenv('LOG')))
         ctx.respond(f'You got an Error: {error}')
         await channel.send(f'Es ist ein Fehler aufgetreten ```{error}```')
         raise error
