5f1b922157f5dcbd0ba81f41ca28ec4f025254bc
BeneX Drake <benex@hotmail.de>
2024-03-16 20:26:02 +0100

    logic works for accept

--- logic/bifroest_logic.py
+++ logic/bifroest_logic.py
@@ -1,8 +1,34 @@
 import discord
-from discord.ext import commands
+from discord import PermissionOverwrite, Role, Member
+
+from cogs.buttons.invite import InviteButtonView
+from database.guilds import Guilds
+
+
 
 class BifroestLogic():
-    #def __init__(self):
+    def __init__(self, bot:discord.Bot):
+        self.bot = bot
+        pass
 
-    async def start(self, ctx:commands.Context):
-        await ctx.respond(self.__class__.__name__)
+    async def create(self):
+        guilds = self.bot.guilds
+
+        for guild in guilds:
+            if "Midgard" in guild.name:
+                await guild.delete()
+                await Guilds().delete(name='Midgard')
+        
+        newGuild = await self.bot.create_guild(name='Midgard')
+        g = self.bot.get_guild(newGuild.id)
+        overwrite = PermissionOverwrite(view_channel=False)
+        for c in g.channels:
+            await c.set_permissions(c.guild.default_role,overwrite=overwrite)    
+        newChannel = await newGuild.create_text_channel(name='Idavall')
+        perms = newChannel.overwrites_for(newChannel.guild.default_role)
+        perms.send_messages = False
+        await newChannel.set_permissions(newChannel.guild.default_role, overwrite=perms)
+        invite = await newChannel.create_invite()
+        await Guilds().update(guild,invite.url)
+        
+        await newChannel.send('Regeln',view=InviteButtonView())
